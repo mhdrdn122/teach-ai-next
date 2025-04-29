f9a0bf20a9fd51380f38aa8a6fca8eb3f8793c76
MHD-HSN-TD <8883597!@mhd>
2025-04-29 11:22:48 +0300

    add All Unit_3 lessons

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body> 
+      <body>
         {children}
       </body>
     </html>
